312201652ce47cab4cbd4d255d3439b11f4b909d
Oscar Morales <58535998+ozk404@users.noreply.github.com>
2023-10-16 11:28:30 -0600

    Add files via upload

--- cert.sh
+++ cert.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(certificados.tecsify.com)
+domains=(edu.tecsify.com)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="oscar@tecsify.com"
